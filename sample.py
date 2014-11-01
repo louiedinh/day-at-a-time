6adcb930fda077b6e1fc82cec5ff81899e7f5860
Louie Dinh <ldinh@athinkingape.com>
2014-11-01 11:29:02 -0700

    Add sample code for sending messages

--- sample.py
+++ sample.py
@@ -7,12 +7,82 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run
 
+import base64
+import email
+from apiclient import errors
+
+from email.mime.text import MIMEText
+
+
+def GetMimeMessage(service, user_id, msg_id):
+  """Get a Message and use it to create a MIME Message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: The ID of the Message required.
+
+  Returns:
+    A MIME Message, consisting of data from Message.
+  """
+  try:
+    message = service.users().messages().get(userId=user_id, id=msg_id,
+                                             format='raw').execute()
+
+    print 'Message snippet: %s' % message['snippet']
+
+    msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+
+    mime_msg = email.message_from_string(msg_str)
+
+    return mime_msg
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+def CreateMessage(sender, to, subject, message_text):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+def SendMessage(service, user_id, message):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print 'Message Id: %s' % message['id']
+    return message
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
 
 # Path to the client_secret.json file downloaded from the Developer Console
 CLIENT_SECRET_FILE = 'client_secret.json'
 
 # Check https://developers.google.com/gmail/api/auth/scopes for all available scopes
-OAUTH_SCOPE = 'https://www.googleapis.com/auth/gmail.readonly'
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/gmail.compose'
 
 # Location of the credentials storage file
 STORAGE = Storage('gmail.storage')
@@ -33,9 +103,13 @@ http = credentials.authorize(http)
 gmail_service = build('gmail', 'v1', http=http)
 
 # Retrieve a page of threads
-threads = gmail_service.users().threads().list(userId='me').execute()
+#messages = gmail_service.users().messages().list(userId='me').execute()
+#
+#for message in messages['messages']:
+#    msg_id = message['id']
+#    print GetMimeMessage(gmail_service, 'me', msg_id)
+#    break
 
-# Print ID for each thread
-if threads['threads']:
-  for thread in threads['threads']:
-    print 'Thread ID: %s' % (thread['id'])
+message = CreateMessage(sender='louiedinh+daily@gmail.com', to='louiedinh@gmail.com', subject='testing', message_text="hi!")
+
+SendMessage(gmail_service, 'me', message)
